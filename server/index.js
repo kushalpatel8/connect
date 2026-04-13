d96852a9e81eb1f3ca07f58c950cd92742a4913e
Kushal Patel <161633671+kushalpatel8@users.noreply.github.com>
2026-04-13 21:51:32 +0530

    Database Connection

--- server/index.js
+++ server/index.js
@@ -1,6 +1,27 @@
 import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+
+dotenv.config();
 
 const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const connectDB = async () => {
+    try {
+        const conn = await mongoose.connect(process.env.MONGODB_URL);
+        console.log(`MongoDB Connected: ${conn.connection.host}`);
+    }
+    catch (error) {
+        console.log('Database connection error:', error.message);
+        process.exit(1);
+    }  
+};
+
+connectDB();
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
